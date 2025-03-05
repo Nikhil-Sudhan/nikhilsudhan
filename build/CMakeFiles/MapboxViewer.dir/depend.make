e05320552b325d60a6f4f047fa6b353bc14eb090
Nikhil-Sudhan <jnspayload@gmail.com>
2025-03-06 00:57:05 +0530

    Replaced existing code with new project

--- build/CMakeFiles/MapboxViewer.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for MapboxViewer.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/MapboxViewer_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/MapboxViewer_autogen"
-  "MapboxViewer_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/MapboxViewer_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
